5da609f9e94f6ee4ce2534eb9e71b73aa906a9d4
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-08-05 20:49:48 +0100

    use plugin-env to load plugins

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,5 +1,11 @@
 import type { Plugin } from "./types";
 
+import path from "path";
+import { createRequire } from "module";
+
+// create a custom require resolver that looks at the node_modules of the plugin-env directory
+const require_from_plugin_env = createRequire(path.resolve(__dirname, "../plugin-env/package.json"));
+
 /**
  * Loads a plugin module dynamically and validates its structure.<br>
  * The module must export a `handle_push` function, and optionally a `display_name` string.
@@ -11,11 +17,11 @@ export const load_plugin = (name: string) => {
     // TODO: this means the module is reloaded on each button press! need a way to detect if the plugin file has changed, and if not then just return the cached version
     // TODO: doing this also allows plugins to do stuff at init reliably, although maybe its easier to do some sort of versioning in the grid data?
     // ^^ we could also validate basic plugin structure when grid data is updated if we known its consistent
-    if (require.cache[require.resolve(name)]) {
-        delete require.cache[require.resolve(name)];
+    if (require_from_plugin_env.cache[require_from_plugin_env.resolve(name)]) {
+        delete require_from_plugin_env.cache[require_from_plugin_env.resolve(name)];
     }
 
-    const plugin = require(name);
+    const plugin = require_from_plugin_env(name);
 
     if (typeof plugin.handle_push !== "function") {
         throw new Error(`Plugin ${name} does not export a handle_push function.`);
